fd2939644fbb7621f4446ce15719791852ccff9c
Sina Beyki <mrsaher83@gmail.com>
2023-08-31 00:12:12 +0330

    SellersData Created

---
+++ src/components/TopSeller/Sellersdata.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    img: "https://api12.iloveimg.com/v1/download/8wxspA82n87y460v9mvwt2lstsyjclwbvmqz8h66vcxcwrq44Aqxhdq19jw03pzc8886Anm21Acshmlsb6m5rvmpArg7dgxybhmjg761qkbkAt1gqx2tkg7hwdtv7wr6h5j0A6w6xqxgb11jk42q7bAx6psqAls2myrybb0ljAtmfAqdvy6q",
+    Name: "Mersedes Bens",
+    Price: "3.5 میلیارد تومان",
+  },
+  {
+    id: 2,
+    img: "https://api11.iloveimg.com/v1/download/yxdk1n7n3k49h4gsm7hf09lhcdcgycqtfAd3tn2tl5h683fdq5m38ymxp6fzl283dsA3h1fmldrwpjjck3d8cz97sx5x0l25hf19d0p9mly3x1f3ld4640j4tcwjb371q0krlt42spbffxt2xq71m487jhmxmhl8k4zw8hlj20zy141n4ppq",
+    Name: "BMW",
+    Price: "1.8 میلیارد تومان",
+  },
+  {
+    id: 3,
+    img: "https://api8.iloveimg.com/v1/download/7dv6fh34m4sdy35ns68v9z5ff5qrAb1hdc38f3lzsdAlk0b1mywrxf7v7xr6zyfj0zm4b2lgp9dk6ly7z8nx4hs4z1cz31p6r3mn4ymqk59nmxvvny5mw5fz6v0pmAqcch8xf5v0b39ql78g6trgs9tngA09fwnns9nv3f3g1kv9jq3v6pc1",
+    Name: "Porsche",
+    Price: "6.2 میلیارد تومان",
+  },
+  {
+    id: 4,
+    img: "https://api3.iloveimg.com/v1/download/52fmlv3q2yb9hzrts6mtfzgjlqp6qsnfgfn1rqymv6zjckkhs3z5h3dtxg5fkw9kfl2vhmw9d2bnkA6k13g2lqz62xw5zkdbxp78r9spfslcp59m2d17skmjv62djc05zvvv7wzlrn2lmbbvwd8myx8458Ayw6x3rjAbtmwldx5sczw536hq",
+    Name: "Ferarri",
+    Price: "11.6 میلیارد تومان",
+  },
+  
+];
+
+export default data;
