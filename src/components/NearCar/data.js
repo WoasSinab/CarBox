8b380cfd5467914c323fbc38aacacc38c37a110c
Sina Beyki <mrsaher83@gmail.com>
2023-08-30 23:00:48 +0330

    some edit in data

---
+++ src/components/NearCar/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    img: "https://s6.uupload.ir/files/1629920_852_rya0.png",
+    Name: "پژو پارس TU5",
+    Miles: "5000  ",
+    Brand: "ایران خودرو",
+    Price: "550 میلیون تومان",
+    Discount: "10%",
+  },
+  {
+    id: 2,
+    img: "https://up.20script.ir/file/f2d9-فوری؛-خبر-مهم-شورای-رقابت-از-تغییر-قیمت-کارخانه-ای-دنا-پلاس.jpg",
+    Name: "دنا پلاس",
+    Miles: "23000  ",
+    Brand: "ایران خودرو",
+    Price: "750 میلیون تومان",
+    Discount: "10%",
+  },
+  {
+    id: 3,
+    img: "https://s6.uupload.ir/files/1629920_852_rya0.png",
+    Name: "شاهین",
+    Miles: "11000  ",
+    Brand: "سایپا",
+    Price: "450 میلیون تومان",
+    Discount: "10%",
+  },
+];
+
+export default data;
