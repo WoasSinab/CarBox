eb724133b2f55e55f08755ffeb657b06b445e20d
Sina Beyki <mrsaher83@gmail.com>
2023-09-06 22:46:37 +0330

    reviews file created

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    img: "https://www.bmw.de/content/dam/bmw/common/all-models/x-series/x6/2023/highlights/bmw-X-series-x6-ms-finance.jpg",
+    CarModel: "BMW X6 Series",
+    Review: "فوق العادس هم از نظر ظاهر هم آپشن واثعا کی میتونه اینو ول کنه شاهین و این چیزا سوار شه؟",
+    UserImg:"",
+    UserName:"",
+    Rate:""
+  },
+  {
+    id: 2,
+    img: "https://www.hamrah-mechanic.com/mag/wp-content/uploads/2022/03/taraaa.jpg?v=1686616279",
+    CarModel: "Tara",
+    Review: "ماشین خیلی خوبی برای ما کارمندا هست و با خرید این ماشین به تولید ملی کمک میکنیم (مالش)",
+    UserImg:"",
+    UserName:"",
+    Rate:""
+  },
+  {
+    id: 3,
+    img: "https://www.radiscar.com/wp-content/uploads/2022/10/s500.jpg",
+    CarModel: "Mersedes Benz S500",
+    Review: "بهترین ماشینی که میتونستم با چشمام ببینم واقعا محشره هرچی بگم بازم کمه کص ننه هیترا",
+    UserImg:"",
+    UserName:"",
+    Rate:""
+  },
+];
+
+export default data;
